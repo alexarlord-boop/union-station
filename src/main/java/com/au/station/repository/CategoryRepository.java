48784a18a332e63a2a4be1f509b1658a63cca87e
l92169 <71372570+l92169@users.noreply.github.com>
2022-05-23 21:17:46 +0300

    Create Cargo and Category Repositories

--- src/main/java/com/au/station/repository/CategoryRepository.java
+++ src/main/java/com/au/station/repository/CategoryRepository.java
@@ -1,4 +1,19 @@
 package com.au.station.repository;
 
+import com.au.station.entity.Cargo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
 public interface CategoryRepository {
+    @Query("select Category from com.au.station.entity.Category as Category")
+    List<Cargo> getAllCategory();
+
+    @Query("select Category from com.au.station.entity.Category as Category " +
+            "where CargoName = :name "
+    )
+    List<Cargo> getCategoryByCargoName(@Param("name") String name);
+
 }
