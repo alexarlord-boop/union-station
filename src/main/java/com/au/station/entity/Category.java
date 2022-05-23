3cafa1fc965709eecc3c1e1e57e257257790271f
l92169 <71372570+l92169@users.noreply.github.com>
2022-05-23 20:52:52 +0300

    Create CategoryEntity

--- src/main/java/com/au/station/entity/Category.java
+++ src/main/java/com/au/station/entity/Category.java
@@ -1,4 +1,33 @@
 package com.au.station.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Category {
+    @Id
+    @Column(name = "Category_ID", nullable = false)
+    public Integer Category_ID;
+
+    @Column(name = "Cargo_Name", nullable = false)
+    public String Cargo_Name;
+
+    public Integer getCategory_ID() {
+        return Category_ID;
+    }
+
+    public void setCategory_ID(Integer category_ID) {
+        Category_ID = category_ID;
+    }
+
+    public String getCargo_Name() {
+        return Cargo_Name;
+    }
+
+    public void setCargo_Name(String cargo_Name) {
+        Cargo_Name = cargo_Name;
+    }
 }
