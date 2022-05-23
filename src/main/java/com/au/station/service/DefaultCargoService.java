194a5d1eb7df7d8941969ab8689c3932d0d3c8b0
l92169 <71372570+l92169@users.noreply.github.com>
2022-05-23 21:37:53 +0300

    Create Cargo and Category Services

--- src/main/java/com/au/station/service/DefaultCargoService.java
+++ src/main/java/com/au/station/service/DefaultCargoService.java
@@ -1,4 +1,20 @@
 package com.au.station.service;
 
+import com.au.station.entity.Cargo;
+import com.au.station.repository.CargoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class DefaultCargoService {
+    private final CargoRepository cargoRepository;
+    @Autowired
+    public DefaultCargoService(CargoRepository cargoRepository) {
+        this.cargoRepository = cargoRepository;
+    }
+
+    public List<Cargo> getAllCargo() {
+        List<Cargo> Cargo = cargoRepository.getAllCargo();
+        return Cargo;
+    }
 }
