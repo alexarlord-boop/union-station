b56ac1d3f19c5d14f472521228ae7118b27454cc
l92169 <71372570+l92169@users.noreply.github.com>
2022-05-23 20:52:31 +0300

    Create CargoEntity

--- src/main/java/com/au/station/entity/Cargo.java
+++ src/main/java/com/au/station/entity/Cargo.java
@@ -1,4 +1,41 @@
 package com.au.station.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Cargo {
+    @Id
+    @Column(name = "Cargo", nullable = false)
+    private Integer Cargo;
+
+    @Column(name = "Category_ID", nullable = false)
+    private Integer Category_ID;
+
+    @Column(name = "Netto", nullable = false)
+    private Integer Netto;
+
+    public Integer getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(Integer cargo) {
+        Cargo = cargo;
+    }
+
+    public Integer getCategory_ID() {
+        return Category_ID;
+    }
+
+    public void setCategory_ID(Integer category_ID) {
+        Category_ID = category_ID;
+    }
+
+    public Integer getNetto() {
+        return Netto;
+    }
+
+    public void setNetto(Integer netto) {
+        Netto = netto;
+    }
 }
