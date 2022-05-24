8fe3b1d1b9a18d2048081e1569e2743280047c9f
ooooda <anya.smirdina@gmail.com>
2022-05-24 14:34:50 +0300

    UNION STATION-2022: Added train classes.

--- src/main/java/com/au/station/entity/Train.java
+++ src/main/java/com/au/station/entity/Train.java
@@ -1,4 +1,65 @@
 package com.au.station.entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
 public class Train {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Train_ID", nullable = false)
+    private Integer trainID;
+
+    @Column(name = "Wagon_List", nullable = false)
+    private Integer[] wagonList;
+
+    @Column(name = "Status", nullable = false)
+    private Integer status;
+
+    @Column(name = "Date_Time", nullable = false)
+    private LocalDateTime dateTime;
+
+    @Column(name = "Next_Station", nullable = false)
+    private Integer nextStation;
+
+    public Integer getTrainID() {
+        return trainID;
+    }
+
+    public void setTrainID(Integer trainID) {
+        this.trainID = trainID;
+    }
+
+    public Integer[] getWagonList() {
+        return wagonList;
+    }
+
+    public void setWagonList(Integer[] wagonList) {
+        this.wagonList = wagonList;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getNextStation() {
+        return nextStation;
+    }
+
+    public void setNextStation(Integer nextStation) {
+        this.nextStation = nextStation;
+    }
 }
--- src/main/java/com/au/station/service/DefaultTrainService.java
+++ src/main/java/com/au/station/service/DefaultTrainService.java
@@ -1,4 +1,27 @@
 package com.au.station.service;
 
+import com.au.station.dto.TrainDto;
+import com.au.station.entity.Train;
+import com.au.station.repository.TrainRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class DefaultTrainService {
+    private final TrainRepository trainRepository;
+    @Autowired
+    public DefaultTrainService(TrainRepository trainRepository) {
+        this.trainRepository = trainRepository;
+    }
+
+    public TrainDto getTrain(Integer id) {
+        Train train = trainRepository.getById(id);
+        return new TrainDto(
+                train.getTrainID(),
+                train.getWagonList(),
+                train.getStatus(),
+                train.getDateTime(),
+                train.getNextStation()
+        );
+    }
 }
