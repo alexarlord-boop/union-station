90e4b1c845119915df2e3f37616b7a6fd59d5372
alexarlord-boop <rezerw0.1@yandex.ru>
2022-05-25 11:38:54 +0300

    STATION-2022 Added dataline to wagon translation method

--- src/main/java/com/au/station/addons/TrainFileParser.java
+++ src/main/java/com/au/station/addons/TrainFileParser.java
@@ -1,12 +1,23 @@
 package com.au.station.addons;
 
 
-
 import com.au.station.dto.TrainDto;
+import com.au.station.dto.WagonDto;
 
 import java.util.List;
 
 public class TrainFileParser {
+    private String data;
+    public List<TrainDto> trains;
 
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    // multithread process
+    public WagonDto dataLineToWagonDto(String dataline){
+        String[] parts = dataline.split(",");
+        return new WagonDto(parts[0], parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+    }
 }
 
