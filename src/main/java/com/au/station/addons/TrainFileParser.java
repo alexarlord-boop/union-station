07e178a48958b287aada1d2b293761289dd66585
alexarlord-boop <rezerw0.1@yandex.ru>
2022-05-27 22:17:55 +0300

    STATION-2022 Added TrainFileParser base realisation

--- src/main/java/com/au/station/addons/TrainFileParser.java
+++ src/main/java/com/au/station/addons/TrainFileParser.java
@@ -4,20 +4,35 @@ package com.au.station.addons;
 import com.au.station.dto.TrainDto;
 import com.au.station.dto.WagonDto;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TrainFileParser {
     private String data;
-    public List<TrainDto> trains;
+    private List<WagonDto> train;
+    private TrainDto trainDto;
 
     public void setData(String data) {
         this.data = data;
     }
 
-    // multithread process
-    public WagonDto dataLineToWagonDto(String dataline){
+    public List<WagonDto> getTrainAsWagonList() {
+        return this.train;
+    }
+
+    public WagonDto dataLineToWagonDto(String dataline) {
         String[] parts = dataline.split(",");
         return new WagonDto(parts[0], parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+    }
+
+    public void dataToWagonList() {
+        List<String> lines = Arrays.asList(this.data.split("\n"));
+        for (String line : lines) {
+            train.add(dataLineToWagonDto(line));
+        }
+    }
+
+    public void dataToTrainDto() {
     }
 }
 
