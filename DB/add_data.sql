480aadb286a4180ecd7c58ff3ec59dd121afa8e3
OcTatiana <71348725+OcTatiana@users.noreply.github.com>
2022-05-28 15:15:02 +0300

    Added data in tables: Cargo_Category, Station

---
+++ DB/add_data.sql
@@ -0,0 +1,38 @@
+---------------------------------------------------------------
+-- Добавление данных в таблицу категории грузов
+---------------------------------------------------------------
+
+INSERT INTO Cargo_Category (Category_ID, Category_Name) VALUES (1, 'Category 1')
+INSERT INTO Cargo_Category (Category_ID, Category_Name) VALUES (2, 'Category 2')
+INSERT INTO Cargo_Category (Category_ID, Category_Name) VALUES (3, 'Category 3')
+INSERT INTO Cargo_Category (Category_ID, Category_Name) VALUES (4, 'Category 4')
+
+
+---------------------------------------------------------------
+-- Добавление данных в таблицу станций
+---------------------------------------------------------------
+
+INSERT INTO Station(Station_ID, Station_Name) VALUES (1, 'Moscow')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (2, 'Vyborg')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (3, 'Pskov')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (4, 'Smolensk')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (5, 'Kazan')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (6, 'Tula')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (7, 'Voronezh')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (8, 'Vologda')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (9, 'Kostroma')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (10, 'Bryansk')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (11, 'Ufa')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (12, 'Dubna')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (13, 'Samara')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (14, 'Saratov')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (15, 'Tver')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (16, 'Volkhov')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (17, 'Kaluga')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (18, 'Perm')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (19, 'Kursk')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (20, 'Arkhangelsk')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (21, 'Syktyvkar')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (22, 'Kirov')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (23, 'Podolsk')
+INSERT INTO Station(Station_ID, Station_Name) VALUES (24, 'Oryol')
